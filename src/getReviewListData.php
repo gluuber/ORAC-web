58a4375d02448ff620ff10e736cfb26d7234ea30
gluuber <boombana@gmail.com>
2026-06-14 18:35:29 +1000

    Initial import

---
+++ src/getReviewListData.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+{
+"data": [
+  ["1","","Sooty Albatross","Phoebetria fusca","Moruya","2 Aug 1987","Not Confirmed (not assessed)"  ],
+  ["2","","Pink Robin","Petroica rodinogaster","Korrungulla Swamp, Primbee","4 Sep 1987","Accepted"  ]
+  ]}
+
+  rid
+  orac_case_number
+  barc_case_number
+  species_name
+  scientific_name
+  location
+  sightingdate
+  decision
+*/
+
+require_once '../mysql.connection.php';
+
+$json_string = '{';
+
+// Create connection
+  $conn = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$json = '{"data": [';
+$query = "SELECT * FROM orac_review_list";
+$result = $conn->query($query);
+if ($result->num_rows > 0) {
+  // output data of each row
+  while ($row = $result->fetch_assoc()) {
+    $json .= '["'. $row["species_name"] . '","'. $row["scientific_name"] . '","'. $row["exemption"] . '"],';
+  }
+}
+$conn->close();
+$json = trim($json, ",");
+$json .= "]}";
+echo $json;
+?>
+
