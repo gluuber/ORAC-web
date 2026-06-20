233c42ac63cf44ffcdea3a6678d34c41283f04f0
gluuber <boombana@gmail.com>
2026-06-21 07:14:55 +1000

    Changed to landscape. Corrected margins and padding

--- index-of-cases-pdf.php
+++ index-of-cases-pdf.php
@@ -18,20 +18,20 @@ $conn = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$html_fragment = '<style>h1 {font-size:14px;}table td {font-size:10px;padding:4px;margin:0px!important;border:1px solid #ccc;}</style>';
+$html_fragment = '<style>table {padding:0px!important;margin:0px!important;width: 100%!important;table-layout: auto!important;border-collapse: collapse;}h1 {font-size:14px;font-family:"Helvetica Neue", Arial, sans-serif;}table td {font-size:10px;font-family:"Helvetica Neue", Arial, sans-serif;margin:0px!important;border:1px solid #ccc;}</style>';
 $query = "SELECT * FROM orac_case_list";
 $result = $conn->query($query);
 $count = 1;
 
 // Header
-$html_fragment .= '<h1>Index of Cases</h1><table><tr><td width="10%"><b>ORAC Case</b></td><td width="10%"><b>BARC Case</b></td><td><b>IOC English name</b></td><td><b>Scientific name</b></td><td><b>Location</b></td><td><b>Sighting date</b></td><td><b>Decision</b></td></tr>';
+$html_fragment .= '<h1>NSW ORAC - Index of Cases</h1><table cellspacing="0" cellpadding="2"><tr><td width="10%" align="center"><b>ORAC Case</b></td><td width="10%" align="center"><b>BARC Case</b></td><td><b>IOC English name</b></td><td><b>Scientific name</b></td><td><b>Location</b></td><td align="center"><b>Sighting date</b></td><td align="center"><b>Decision</b></td></tr>';
 
 // Body
 if ($result->num_rows > 0) {
   // output data of each row
   while ($row = $result->fetch_assoc()) {
     $date = date_create($row["sightingdate"]);
-    $html_fragment .= '<tr><td>' . $row["orac_case_number"] . '</td><td>' . $row["barc_case_number"] . '</td><td>' . $row["species_name"] . '</td><td><i>' . $row["scientific_name"] . '</i></td><td>' . $row["location"] . '</td><td>' . date_format($date, 'j M Y') . '</td><td>' . $row["decision"] . '</td></tr>';
+    $html_fragment .= '<tr><td align="center">' . $row["orac_case_number"] . '</td><td align="center">' . $row["barc_case_number"] . '</td><td>' . $row["species_name"] . '</td><td><i>' . $row["scientific_name"] . '</i></td><td>' . $row["location"] . '</td><td align="center">' . date_format($date, 'j M Y') . '</td><td align="center">' . $row["decision"] . '</td></tr>';
     $count++;
     //if ($count == 700) {
     //  break;
@@ -42,13 +42,8 @@ $html_fragment .= '</table>';
 
 $conn->close();
 $dompdf->loadHtml($html_fragment);
-
-// (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'portrait');
-
-// Render the HTML as PDF
+$dompdf->setPaper('A4', 'landscape');
 $dompdf->render();
-
-// Output the generated PDF to Browser
 $dompdf->stream('NSW ORAC - Index of Cases');
+//echo $html_fragment;
 ?>
